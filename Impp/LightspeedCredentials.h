f77d12d4c4ee4cc3fffafefc968c2a580178ebbf
Wan, peng <pewan@stubhub.com>
2015-08-19 22:29:10 -0700

    new code

---
+++ Impp/LightspeedCredentials.h
@@ -0,0 +1,14 @@
+//
+//  LightspeedCredentials.h
+//  IMChat
+//
+//  Created by Jefferson on 2015/1/8.
+//  Copyright (c) 2015年 Herxun. All rights reserved.
+//
+
+#ifndef IMChat_LightspeedCredentials_h
+#define IMChat_LightspeedCredentials_h
+
+#define LIGHTSPEED_APP_KEY @"TvDkvPMjRlnbTbsHJ4BCsgo0ozro8OPf"
+#define LIGHTSPEED_WALL_ID @"55d4f07a8fe5560000000137"
+#endif
--- Podfile
+++
@@ -1,4 +0,0 @@
-platform :ios, '7.0'
-pod 'AFNetworking', '~> 2.4.1'
-pod 'BugSense'
-pod 'DCPathButton', '~> 2.0'
